5b2d6cc090f31af5a8114342c9fb04dd1902dfd4
migfgbr <alexlele2@gmail.com>
2026-02-06 16:22:25 -0400

    Update main.py
    sim

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-meme_dict = {
-    "gankado": "ter sido alvo de um ataque surpresa ou emboscada",
-    "lore": "história",
-    "hype": "entusiasmo",
-    "easter egg" : "mensagem ou recurso escondido",
-    "rage": "pessoa entra em estado de fúria e frustração"
-}
-print(meme_dict)
-doubt = input("qual dessas palavras você quer saber?")
-if doubt == "gankado":
-    print(meme_dict["gankado"])
-elif doubt == "lore":
-    print(meme_dict["lore"])
-elif doubt == "hype":
-    print(meme_dict["hype"])
-elif doubt == "easter egg":
-    print(meme_dict["easter egg"])
-elif doubt == "rage":
-    print(meme_dict["rage"])
-else:
-    print("não conheço essa palavra :| ")
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+    bot.run("Token')
